cfc7bf783080df1af84b3e786a6889c42d64c2f5
Leather128 <81660872+Leather128@users.noreply.github.com>
2023-05-03 18:15:35 -0400

    fuck fuck fufck udfjogidf

--- src/GamePlugin/Core/SpriteXML/mod.rs
+++ src/GamePlugin/Core/SpriteXML/mod.rs
@@ -1,12 +1,11 @@
 use bevy::prelude::*;
 use serde::{Deserialize, Serialize};
-use serde_xml_rs::{from_str};
+use serde_xml_rs::from_str;
 
 // XML Format Serialization
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
-struct SubTexture
-{
+struct SubTexture {
     pub name: String,
     pub x: u32,
     pub y: u32,
@@ -23,119 +22,128 @@ struct SubTexture
     #[serde(default = "default_u32")]
     pub frameWidth: u32,
     #[serde(default = "default_u32")]
-    pub frameHeight: u32
-
+    pub frameHeight: u32,
 }
 
-fn default_i32() -> i32
-{
+fn default_i32() -> i32 {
     0
 }
-fn default_u32() -> u32
-{
+fn default_u32() -> u32 {
     0
 }
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub struct XML
-{
+pub struct XML {
     #[serde(rename = "SubTexture")]
-    subtexture: Vec<SubTexture>
+    subtexture: Vec<SubTexture>,
 }
 
 #[derive(Clone)]
-pub struct Offsets
-{
-    pub offsets_vec: Vec<Vec2>
+pub struct Offsets {
+    pub offsets_vec: Vec<Vec2>,
 }
 
 #[derive(Component, Clone)]
-pub struct SpriteXML
-{
-    pub offsets: Offsets
-
+pub struct SpriteXML {
+    pub offsets: Offsets,
 }
 
 #[derive(Bundle, Clone)]
-pub struct SpriteXMLBundle
-{
+pub struct SpriteXMLBundle {
     pub sprite_sheet: SpriteSheetBundle,
-    pub spritexml: SpriteXML
-    
+    pub spritexml: SpriteXML,
 }
 
-impl SpriteXMLBundle
-{
-    pub fn new(xml_path: String, atlas_handle: &Handle<TextureAtlas>, modify: &mut TextureAtlas) -> Option<Self>
-    {
+impl SpriteXMLBundle {
+    pub fn new(
+        xml_path: String,
+        atlas_handle: &Handle<TextureAtlas>,
+        modify: &mut TextureAtlas,
+    ) -> Option<Self> {
         let file = std::fs::read_to_string(xml_path);
 
-        match file
-        {
-            Ok(f) =>
-            {
+        match file {
+            Ok(f) => {
                 let x = f.replace("\u{feff}", "");
                 let xml: Result<XML, serde_xml_rs::Error> = from_str(&x);
-                match xml
-                {
-                    Ok(xml) => 
-                    {
-                        let sheet_bundle = SpriteSheetBundle {sprite: TextureAtlasSprite::new(0), texture_atlas: atlas_handle.clone(), ..default()};
-                        let mut temp_offsets: Offsets = Offsets { offsets_vec: Vec::new() };
+                match xml {
+                    Ok(xml) => {
+                        let sheet_bundle = SpriteSheetBundle {
+                            sprite: TextureAtlasSprite::new(0),
+                            texture_atlas: atlas_handle.clone(),
+                            ..default()
+                        };
+                        let mut temp_offsets: Offsets = Offsets {
+                            offsets_vec: Vec::new(),
+                        };
 
-                        for texture in xml.subtexture.iter()
-                        {
+                        for texture in xml.subtexture.iter() {
                             let name = &texture.name;
 
-                            let rect = Rect::new(texture.x as f32, texture.y as f32, texture.x as f32 + texture.width as f32, texture.y as f32 + texture.height as f32);
-                            if texture.frameX != 0
-                            {
-                                temp_offsets.offsets_vec.push(Vec2::new(texture.frameX as f32, texture.frameY as f32));
+                            if !name.starts_with("boyfriend attack") {
+                                continue;
                             }
-                            else 
-                            {
-                                temp_offsets.offsets_vec.push(Vec2::new(0.0, 0.0));
-                            }
+
+                            let rect = Rect::new(
+                                texture.x as f32,
+                                texture.y as f32,
+                                texture.x as f32 + texture.width as f32,
+                                texture.y as f32 + texture.height as f32,
+                            );
+
+                            temp_offsets
+                                .offsets_vec
+                                .push(Vec2::new(-texture.frameX as f32, texture.frameY as f32));
+
                             modify.add_texture(rect);
                         }
                         // All done!
-                        return Some(
-                            SpriteXMLBundle { sprite_sheet: sheet_bundle, spritexml: SpriteXML { offsets: temp_offsets } }
-                        );
-                    },
-                    Err(e) => 
-                    {
+                        return Some(SpriteXMLBundle {
+                            sprite_sheet: sheet_bundle,
+                            spritexml: SpriteXML {
+                                offsets: temp_offsets,
+                            },
+                        });
+                    }
+                    Err(e) => {
                         println!("ERROR: {}", e);
-                    },
+                    }
                 }
-            },
-            Err(e) =>
-            {
+            }
+            Err(e) => {
                 println!("ERROR: {}", e);
                 return None;
             }
         }
         return None;
     }
-
-    
 }
 
-impl SpriteXML
-{
-    pub fn get_next_frame(&mut self, sprite: &mut TextureAtlasSprite, translation: &mut Transform)
-    {
+impl SpriteXML {
+    pub fn get_next_frame(&mut self, sprite: &mut TextureAtlasSprite, translation: &mut Transform) {
         let index = sprite.index;
 
-        if index < self.offsets.offsets_vec.len() - 1
-        {
-            // it's in bounds add ze offset
-            let offset = self.offsets.offsets_vec[index];
-            if index != 0
-            {
-                translation.translation -= Vec3::new(self.offsets.offsets_vec[index - 1].x, self.offsets.offsets_vec[index - 1].y, 0.0);
-            }
-            sprite.index += 1;
-            translation.translation += Vec3::new(offset.x, offset.y, 0.0);
+        if index >= self.offsets.offsets_vec.len() - 1 {
+            sprite.index = 0;
+
+            translation.translation -= Vec3::new(
+                self.offsets.offsets_vec[self.offsets.offsets_vec.len() - 1].x,
+                self.offsets.offsets_vec[self.offsets.offsets_vec.len() - 1].y,
+                0.0,
+            );
         }
+
+        // it's in bounds add ze offset
+        let offset = self.offsets.offsets_vec[index];
+
+        if index != 0 {
+            translation.translation -= Vec3::new(
+                self.offsets.offsets_vec[index - 1].x,
+                self.offsets.offsets_vec[index - 1].y,
+                0.0,
+            );
+        }
+
+        sprite.index += 1;
+        translation.translation += Vec3::new(offset.x, offset.y, 0.0);
     }
 }
--- src/GamePlugin/PreLoader/mod.rs
+++ src/GamePlugin/PreLoader/mod.rs
@@ -1,38 +1,35 @@
 use std::time::Duration;
 
-use bevy::prelude::*;
+use super::GameState;
 use bevy::core_pipeline::clear_color::ClearColorConfig;
+use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
 use bevy_kira_audio::AudioSource;
+use bevy_tweening::lens::{SpriteColorLens, TransformScaleLens};
 use bevy_tweening::*;
-use bevy_tweening::lens::{TransformScaleLens, SpriteColorLens};
-use super::GameState;
 
 pub struct PreloaderPlugin;
 
-impl Plugin for PreloaderPlugin
-{
+impl Plugin for PreloaderPlugin {
     fn build(&self, app: &mut App) {
-        app
-            .add_loading_state(LoadingState::new(GameState::ClockAsset).continue_to_state(GameState::Preload))
-            .add_loading_state(LoadingState::new(GameState::Preload).continue_to_state(GameState::Intro))
-            .add_collection_to_loading_state::<_, PreloadClockAsset>(GameState::ClockAsset)
-            .add_collection_to_loading_state::<_, PreloadFunkinAssets>(GameState::Preload)
-            .add_system(PreLoaderInit.in_schedule(OnEnter(GameState::Preload)))
-            .add_systems(
-                (
-                    TickClockTimer,
-                    Clock,
-                    take_care_of_clock
-                ).in_set(OnUpdate(GameState::Preload))
-            )
-            .add_system(ByeByePreloader.in_schedule(OnExit(GameState::Preload)))
-            .add_system(GetRidOfClock.in_set(OnUpdate(GameState::Intro)));
+        app.add_loading_state(
+            LoadingState::new(GameState::ClockAsset).continue_to_state(GameState::Preload),
+        )
+        .add_loading_state(
+            LoadingState::new(GameState::Preload).continue_to_state(GameState::Intro),
+        )
+        .add_collection_to_loading_state::<_, PreloadClockAsset>(GameState::ClockAsset)
+        .add_collection_to_loading_state::<_, PreloadFunkinAssets>(GameState::Preload)
+        .add_system(PreLoaderInit.in_schedule(OnEnter(GameState::Preload)))
+        .add_systems(
+            (TickClockTimer, Clock, take_care_of_clock).in_set(OnUpdate(GameState::Preload)),
+        )
+        .add_system(ByeByePreloader.in_schedule(OnExit(GameState::Preload)))
+        .add_system(GetRidOfClock.in_set(OnUpdate(GameState::Intro)));
     }
 }
 #[derive(AssetCollection, Resource)]
-pub struct PreloadFunkinAssets
-{
+pub struct PreloadFunkinAssets {
     // currently the loading time to load the song on the debug build is ABSOLUTELY UNEXPECTABLE
     // but here's the issue, the bevy_kira_plugin has no support for streaming audio
     // therefore its slow AF
@@ -45,23 +42,20 @@ pub struct PreloadFunkinAssets
     pub freaky: Handle<AudioSource>,
     #[asset(path = "images/BOYFRIEND.png")]
     pub bf: Handle<Image>,
-    
 }
 #[derive(AssetCollection, Resource)]
-pub struct PreloadClockAsset
-{
+pub struct PreloadClockAsset {
     #[asset(path = "images/clock.png")]
-    clock: Handle<Image>
+    clock: Handle<Image>,
 }
 #[derive(Component)]
 pub struct ClockTimer(Timer);
 
 #[derive(Bundle)]
-pub struct ClockTweens
-{
+pub struct ClockTweens {
     scale: Animator<Transform>,
     alpha: Animator<Sprite>,
-    idenifier: ClockGhost
+    idenifier: ClockGhost,
 }
 #[derive(Component)]
 pub struct ClockGhost(); // used to identify the effect clock and delete it after use
@@ -69,61 +63,72 @@ pub fn PreLoaderInit(
     mut commands: Commands,
     clock_img: Res<PreloadClockAsset>,
     mut settings: ResMut<bevy_framepace::FramepaceSettings>,
-)
-{
+) {
     settings.limiter = bevy_framepace::Limiter::from_framerate(24.0);
-    let tween = Tween::new(EaseFunction::SineInOut, Duration::from_millis(1000), TransformScaleLens {start: Vec3::new(1.0, 1.0, 1.0), end: Vec3::new(2., 2., 2.)});
-    let tween2 = Tween::new(EaseFunction::SineInOut, Duration::from_millis(900), SpriteColorLens {start: Color::rgba(1., 1., 1., 1.), end: Color::rgba(1., 1., 1., 0.)});
+    let tween = Tween::new(
+        EaseFunction::SineInOut,
+        Duration::from_millis(1000),
+        TransformScaleLens {
+            start: Vec3::new(1.0, 1.0, 1.0),
+            end: Vec3::new(2., 2., 2.),
+        },
+    );
+    let tween2 = Tween::new(
+        EaseFunction::SineInOut,
+        Duration::from_millis(900),
+        SpriteColorLens {
+            start: Color::rgba(1., 1., 1., 1.),
+            end: Color::rgba(1., 1., 1., 0.),
+        },
+    );
     // Spawn a Basic Camera, Nothing Fancy just for UI
 
-    commands.spawn(Camera2dBundle
-    {
-        projection: OrthographicProjection
-        {
-            scaling_mode: bevy::render::camera::ScalingMode::Fixed { width: 1280., height: 720. }, // TODO: Allow this to be variable in the future.
+    commands.spawn(Camera2dBundle {
+        projection: OrthographicProjection {
+            scaling_mode: bevy::render::camera::ScalingMode::Fixed {
+                width: 1280.,
+                height: 720.,
+            }, // TODO: Allow this to be variable in the future.
             ..default()
         },
-        camera: Camera
-        {
+        camera: Camera {
             order: 1,
             ..default()
         },
-        camera_2d: Camera2d
-        {
-            
+        camera_2d: Camera2d {
             clear_color: ClearColorConfig::Custom(Color::BLACK),
             ..default()
         },
         ..default()
     });
-    commands.spawn((SpriteBundle
-    {
-        texture: clock_img.clock.clone(),
-        transform: Transform::from_xyz((1280.0 / 2.0) - 50.0, (720.0 / 2.0) - 50.0, 0.0).with_scale(Vec3::new(0.5, 0.5, 0.5)),
-        ..default()
-    }, ClockTimer(Timer::from_seconds(1.5, TimerMode::Repeating)))).with_children(|paren|
-    {
-        paren.spawn(
-            (
-                SpriteBundle
-                {
+    commands
+        .spawn((
+            SpriteBundle {
+                texture: clock_img.clock.clone(),
+                transform: Transform::from_xyz((1280.0 / 2.0) - 50.0, (720.0 / 2.0) - 50.0, 0.0)
+                    .with_scale(Vec3::new(0.5, 0.5, 0.5)),
+                ..default()
+            },
+            ClockTimer(Timer::from_seconds(1.5, TimerMode::Repeating)),
+        ))
+        .with_children(|paren| {
+            paren.spawn((
+                SpriteBundle {
                     texture: clock_img.clock.clone(),
                     ..default()
                 },
-                ClockTweens {alpha: Animator::new(tween2), scale: Animator::new(tween), idenifier: ClockGhost()}
-            )
-        );
-    });
+                ClockTweens {
+                    alpha: Animator::new(tween2),
+                    scale: Animator::new(tween),
+                    idenifier: ClockGhost(),
+                },
+            ));
+        });
     println!("{}: Preloading Assets, Please Wait...", file!());
 }
 
-pub fn TickClockTimer(
-    mut query: Query<&mut ClockTimer, With<ClockTimer>>,
-    time: Res<Time>
-)
-{
-    for mut timer in query.iter_mut()
-    {
+pub fn TickClockTimer(mut query: Query<&mut ClockTimer, With<ClockTimer>>, time: Res<Time>) {
+    for mut timer in query.iter_mut() {
         timer.0.tick(time.delta());
     }
 }
@@ -131,70 +136,71 @@ pub fn TickClockTimer(
 pub fn Clock(
     mut commands: Commands,
     query: Query<(&ClockTimer, Entity), With<ClockTimer>>,
-    clock_img: Res<PreloadClockAsset>
-)
-{
-    
-    for (tim, entit) in query.iter()
-    {
-        if tim.0.just_finished()
-        {
-            let tween = Tween::new(EaseFunction::SineInOut, Duration::from_millis(1000), TransformScaleLens {start: Vec3::new(1.0, 1.0, 1.0), end: Vec3::new(2., 2., 2.)}).with_completed_event(0);
-            let tween2 = Tween::new(EaseFunction::SineInOut, Duration::from_millis(900), SpriteColorLens {start: Color::rgba(1., 1., 1., 1.), end: Color::rgba(1., 1., 1., 0.)}).with_completed_event(0);
-            commands.entity(entit).with_children(|parent| 
-                {
-                    parent.spawn((
-                        SpriteBundle
-                        {
-                            texture: clock_img.clock.clone(),
-                            ..default()
-                        },
-                        ClockTweens {alpha: Animator::new(tween2), scale: Animator::new(tween), idenifier: ClockGhost()}
-                    ));
-                }
-            );
+    clock_img: Res<PreloadClockAsset>,
+) {
+    for (tim, entit) in query.iter() {
+        if tim.0.just_finished() {
+            let tween = Tween::new(
+                EaseFunction::SineInOut,
+                Duration::from_millis(1000),
+                TransformScaleLens {
+                    start: Vec3::new(1.0, 1.0, 1.0),
+                    end: Vec3::new(2., 2., 2.),
+                },
+            )
+            .with_completed_event(0);
+            let tween2 = Tween::new(
+                EaseFunction::SineInOut,
+                Duration::from_millis(900),
+                SpriteColorLens {
+                    start: Color::rgba(1., 1., 1., 1.),
+                    end: Color::rgba(1., 1., 1., 0.),
+                },
+            )
+            .with_completed_event(0);
+            commands.entity(entit).with_children(|parent| {
+                parent.spawn((
+                    SpriteBundle {
+                        texture: clock_img.clock.clone(),
+                        ..default()
+                    },
+                    ClockTweens {
+                        alpha: Animator::new(tween2),
+                        scale: Animator::new(tween),
+                        idenifier: ClockGhost(),
+                    },
+                ));
+            });
         }
     }
 }
 
-pub fn take_care_of_clock(
-    mut commands: Commands,
-    mut reader: EventReader<TweenCompleted>
-)
-{
-    for event in reader.iter()
-    {
+pub fn take_care_of_clock(mut commands: Commands, mut reader: EventReader<TweenCompleted>) {
+    for event in reader.iter() {
         commands.entity(event.entity).despawn();
     }
 }
 
-pub fn ByeByePreloader(mut commands: Commands, query: Query<Entity, With<ClockTimer>>)
-{
-    for entity in query.iter()
-    {
+pub fn ByeByePreloader(mut commands: Commands, query: Query<Entity, With<ClockTimer>>) {
+    for entity in query.iter() {
         let tween = Tween::new(
             EaseFunction::QuinticInOut,
             Duration::from_millis(900),
-            SpriteColorLens
-            {
+            SpriteColorLens {
                 start: Color::rgba(1.0, 1.0, 1.0, 1.0),
-                end: Color::rgba(1.0, 1.0, 1.0, 0.0)
-            }
-        ).with_completed_event(1);
+                end: Color::rgba(1.0, 1.0, 1.0, 0.0),
+            },
+        )
+        .with_completed_event(1);
         commands.entity(entity).insert(Animator::new(tween));
-        
     }
 }
 
 // This is stupid but IDC!
-pub fn GetRidOfClock(mut commands: Commands, mut reader: EventReader<TweenCompleted>)
-{
-    for ev in reader.iter()
-    {
-        if ev.user_data == 1
-        {
+pub fn GetRidOfClock(mut commands: Commands, mut reader: EventReader<TweenCompleted>) {
+    for ev in reader.iter() {
+        if ev.user_data == 1 {
             commands.entity(ev.entity).despawn();
-            
         }
     }
 }
